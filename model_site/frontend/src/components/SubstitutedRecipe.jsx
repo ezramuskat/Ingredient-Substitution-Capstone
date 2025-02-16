c2afae68c3ce7e328d291453b165a5267d380541
Ezra Muskat <ezramuskat@gmail.com>
2025-02-16 18:13:55 -0500

    added some stuff that accidentally wasn't added to git

---
+++ model_site/frontend/src/components/SubstitutedRecipe.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+function SubstitutedRecipe(props) {
+	const [recipe, setRecipe] = useState({
+		ingredients: [""],
+	});
+	
+	useEffect(() => {
+		fetch("/api/processed-recipe").then((response) => {
+			if (response.ok) {
+				console.log("Response:", response);
+				response.json().then((contents) => {
+					console.log("Contents:", contents);
+					setRecipe({
+						ingredients: contents.ingredients,
+					});
+				});
+			}
+		});
+		console.log("Recipe Data:", recipe);
+	});
+
+	return (
+		<div className="recipe-form-flex-space">
+			<div className="restrictions-container">
+				{/** blank column for formatting purposes, we can potentially put other stuff here later */}
+			</div>
+
+			<div className="ingredients-container">
+				{recipe.ingredients.map((text, index) => (
+					<div key={index} className="ingredient-row">
+						<p>{text}</p>
+					</div>
+				))}
+			</div>
+
+			<div className="instructions-container">
+				<p>Instructions go here.</p>
+			</div>
+		</div>
+	);
+
+
+}
+
+export default SubstitutedRecipe
