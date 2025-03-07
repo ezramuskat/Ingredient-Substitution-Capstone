3f2e15c3479227c128722078b7070e491e51874c
TuvyaMacklin <tuvyamacklin@gmail.com>
2025-03-06 19:42:15 -0500

    started setting up environment setup

---
+++ setup.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+
+PYTHON_VERSION=3.11
+
+# Create a virtual environment
+VENV_DIR=.venv
+python$PYTHON_VERSION -m venv $VENV_DIR
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Setup complete
+echo "Setup complete! Run 'source $VENV_DIR/bin/activate' to activate your environment."
