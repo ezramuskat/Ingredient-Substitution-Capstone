a4ccd6add2a5b2302a3f544295a24d7fb5d102ab
Ezra Muskat <ezramuskat@gmail.com>
2025-04-17 22:44:34 -0400

    trying out deployment fix

--- startup_prod.sh
+++ startup_prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Start backend
-flask --app app.py run -h 0.0.0.0 -p 5000 &
-FLASK_PID=$!
-
 npm install --prefix frontend
 # Start frontend
 npm run --prefix frontend build &
 REACT_PID=$!
+
+# Start backend
+flask --app app.py run -h 0.0.0.0 -p 5000 &
+FLASK_PID=$!
 
 # Trap to kill both processes on exit
 trap "kill $FLASK_PID $REACT_PID" SIGINT SIGTERM EXIT
